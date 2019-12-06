6c8d29a702a3514b698e7dd5cf3186054bd42790
Iyer Gopi <IyerGopi@JohnDeere.com>
2019-12-06 12:40:10 -0600

    fix: removed getresults for await

--- showcase/src/Program.cs
+++ showcase/src/Program.cs
@@ -1,25 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Showcase {
     public class Program {
-        static void Main (string[] args) {
+        static async Task Main (string[] args) {
+            Task.Delay (0).GetAwaiter ().GetResult ();
             Console.WriteLine ("Enter the Album Number: ");
             string inputAlbumId;
             inputAlbumId = Console.ReadLine ();
             Showcase.Validate v = new Validate ();
 
             if (!v.IsAlbumValid (inputAlbumId)) { Console.WriteLine ("Enter a valid Album Number. The Album Number should be numeric."); } else {
-                PrintPhotos (inputAlbumId);
+               await  PrintPhotos (inputAlbumId);
             }
+            Console.WriteLine ("Ending the program");            
 
         }
 
-        static void PrintPhotos (String input) {
+        static async Task PrintPhotos (String input) {
             Photos showcase = new Photos ();
             int albumId;
             int.TryParse (input, out albumId);
-            var photos = showcase.Get (albumId).Result;
+            var photos = await showcase.Get (albumId);
             if (photos != null)
                 Console.WriteLine ("Fetching all photos for Album #:  " + Convert.ToString (albumId));
             FormatOutput f = new FormatOutput ();
